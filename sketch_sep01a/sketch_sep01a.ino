fc8166ea3b3246a4539bb7d129521639337b73b1
Nonthwatzaza <Nonth2000@gmail.com>
2018-09-01 11:41:23 +0700

    FIX

---
+++ sketch_sep01a/sketch_sep01a.ino
@@ -0,0 +1,20 @@
+#define DO 16
+#define LED 16 //header//
+
+void setup()
+  {
+    pinMode(LED,OUTPUT); 
+  }                     //fact 1
+
+  void loop()
+  {
+    digitalWrite (LED,HIGH);
+    delay(1000);
+    digitalWrite (LED,LOW);
+    delay(1000);
+  }                     //fact2
+
+
+
+
+  
